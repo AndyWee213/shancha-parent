b87f2d01e1fc56f72e16d2b397aebe3a0534d757
weiq <weiq.sp@fosun.com>
2018-03-01 10:46:47 +0800

    Add Config Server and Config repo.

---
+++ shancha-config-server/src/main/java/com/shancha/config/server/ShanchaConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.shancha.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+
+@EnableConfigServer
+@EnableEurekaClient
+@Configuration
+@EnableAutoConfiguration
+public class ShanchaConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShanchaConfigServerApplication.class, args);
+    }
+}
